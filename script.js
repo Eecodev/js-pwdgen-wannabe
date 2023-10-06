4e33b1068df6c0d1b012626f03309dbb92e021f5
Eecodev <mnasser.bbk@gmail.com>
2023-10-06 16:23:13 +0200

    initial scaffolding

---
+++ script.js
@@ -0,0 +1,18 @@
+// Chiedi all'utente il suo nome
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito
+
+// const fixedNumber = 21;
+
+let nome = prompt('Dimmi il tuo nome');
+console.log(nome);
+
+let cognome = prompt('Dimmi il tuo cognome');
+console.log(cognome);
+
+let colore = prompt('Dimmi il tuo colore');
+console.log(colore);
+
+let password = nomecognomecolorepreferito;
+password = nome + '' + cognome + '' + colore;
