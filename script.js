f70fba52a8eb15e020c79e7d75c382a4bc700bb0
Eecodev <mnasser.bbk@gmail.com>
2023-10-06 16:59:14 +0200

    changed let with const

--- script.js
+++ script.js
@@ -4,12 +4,9 @@
 // Infine scrivi sulla pagina nomecognomecolorepreferito
 
 const nome = prompt('quale è il tuo nome ?');
-console.log(nome);
 
 const cognome = prompt('quale è il tuo cognome ?');
-console.log(cognome);
 
-let colore = prompt('quale è il tuo colore preferito ?');
-console.log(colore);
+const colore = prompt('quale è il tuo colore preferito ?');
 
-console.log('nome' + 'cognome' + 'colore' + '21');
+console.log(`nomecognomecolore21`);
